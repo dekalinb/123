5f445bf77b0c99ac5d10c6be6208fe8e0bd86e96
Bohdan <dekalin@gmail.com>
2022-12-28 23:24:14 +0200

    Initial Commit

---
+++ 123/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  123
+//
+//  Created by Bohdan on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
